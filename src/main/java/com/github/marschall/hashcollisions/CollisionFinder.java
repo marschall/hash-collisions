33e4e52718e0a54f7b6848a843e5e9c3b3ba1bfe
Philippe Marschall <philippe.marschall@gmail.com>
2015-10-31 14:19:18 +0100

    bump

---
+++ src/main/java/com/github/marschall/hashcollisions/CollisionFinder.java
@@ -0,0 +1,107 @@
+package com.github.marschall.hashcollisions;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+import com.gs.collections.api.map.primitive.MutableIntIntMap;
+import com.gs.collections.api.map.primitive.MutableIntObjectMap;
+import com.gs.collections.api.set.MutableSet;
+import com.gs.collections.api.tuple.primitive.IntIntPair;
+import com.gs.collections.impl.factory.Sets;
+import com.gs.collections.impl.factory.primitive.IntIntMaps;
+import com.gs.collections.impl.factory.primitive.IntObjectMaps;
+
+public class CollisionFinder {
+  // https://github.com/emboss/schadcode
+
+  public static void main(String[] args) {
+    //findByCountOnly();
+    findByhashCode(1216347);
+//    System.out.printf("%04H%n", 127);
+  }
+
+  private static String toHex(char c) {
+    String hexString = Integer.toHexString(c);
+    switch (hexString.length()) {
+      case 4:
+        return hexString;
+      case 3:
+        return "0" + hexString;
+      case 2:
+        return "00" + hexString;
+      case 1:
+        return "000" + hexString;
+      default:
+        throw new IllegalArgumentException("unsupported string: " + hexString);
+    }
+  }
+
+  private static void findByhashCode(int hashCode) {
+
+    MutableSet<String> collisions = Sets.mutable.empty();
+    for (int i = 0; i < Character.MAX_VALUE; i++) {
+      for (int j = 0; j < Character.MAX_VALUE; j++) {
+        String string = new String(new char[]{(char) i, (char) j});
+        if (string.hashCode() == hashCode) {
+          collisions.add(string);
+        }
+
+      }
+
+    }
+
+    System.out.println(collisions.size());
+    for (String collision : collisions) {
+      System.out.println("\"\\u" + toHex(collision.charAt(0)) +"\\u" + toHex(collision.charAt(0)) + "\",");
+    }
+  }
+
+  private static void findByCountOnly() {
+    MutableIntIntMap counts = IntIntMaps.mutable.empty();
+    //IntStream chars = IntStream.rangeClosed(0, Character.MAX_VALUE);
+    for (int i = 0; i < Character.MAX_VALUE; i++) {
+      for (int j = 0; j < Character.MAX_VALUE; j++) {
+        String string = new String(new char[]{(char) i, (char) j});
+        int key = string.hashCode();
+        int count = counts.getIfAbsent(key, 0);
+        counts.put(key, count + 1);
+      }
+
+    }
+
+    int maxCount = 0;
+    int maxHashCode = 0;
+    Iterator<IntIntPair> iterator = counts.keyValuesView().iterator();
+    while (iterator.hasNext()) {
+      IntIntPair pair = iterator.next();
+      int hashCode = pair.getOne();
+      int count = pair.getTwo();
+      if (count > maxCount) {
+        maxCount = count;
+        maxHashCode = hashCode;
+      }
+    }
+    System.out.println("hashCode: " + maxHashCode + " count: " + maxCount);
+  }
+
+  private static void findAscii() {
+    MutableIntObjectMap<List<String>> counts = IntObjectMaps.mutable.empty();
+    //IntStream chars = IntStream.rangeClosed(0, Character.MAX_VALUE);
+    for (int i = 0; i < 255; i++) {
+      for (int j = 0; j < 255; j++) {
+        for (int k = 0; k < 255; k++) {
+          String string = new String(new char[]{(char) i, (char) j, (char) k});
+          List<String> list = counts.getIfAbsentPut(string.hashCode(), () -> new ArrayList<>(1));
+          list.add(string);
+        }
+      }
+
+    }
+
+    int max = counts.keySet().max();
+    List<String> collisions = counts.get(max);
+    System.out.println(max + ": " + collisions + " (" + collisions.size() + ")");
+  }
+
+}
